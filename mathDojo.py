54cdf9af9e1334b0b70ff194840737710ce707b9
tycoon87 <tyson.woodruff@outlook.com>
2017-09-19 10:28:45 -0700

    not finished

--- mathDojo.py
+++ mathDojo.py
@@ -1,9 +1,35 @@
 class math(object):
-    def __init__(self,add,subtract):
+    def __init__(self,add,subtract,num):
         self.add = add
         self.subtract = subtract
+        self.num = num
         
-    def add(self, x, y):
-        x + y
-math.add(2,3)
-        
+    def add(self, x, y, args):
+        num = x + y
+        return self
+math
+add(2,3) 
+print num
+##        
+##**************************************************
+##create an instance
+#class MathDojo(object):
+##    initial the instance
+#    def __init__(self):
+##        set instance variable
+#        self.result = 0
+##    create a function for the instance
+#    def add(self, *args):
+##        for loop for "args"
+#        for i in args:
+##            if the type of i is a list or if type i is a tuple
+#            if type(i) == list or type(i) == tuple:
+##                start nested for loop
+#                for k in i:
+##                    set result to be k
+#                    self.result += k
+##            outherwise
+#            else:
+##                set result to be i
+#                self.result += i
+#        return self
