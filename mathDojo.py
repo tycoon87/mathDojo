57ec860652b9a308173eb5ec0c79b7cc97bcc405
tycoon87 <tyson.woodruff@outlook.com>
2017-09-19 09:33:21 -0700

    not finished

---
+++ mathDojo.py
@@ -0,0 +1,9 @@
+class math(object):
+    def __init__(self,add,subtract):
+        self.add = add
+        self.subtract = subtract
+        
+    def add(self, x, y):
+        x + y
+math.add(2,3)
+        
